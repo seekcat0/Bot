7020b6723f9f67144d4a8728b43502bf3b5c50c3
Seek Cat <seekcat033@gmail.com>
2024-05-30 13:09:27 +0700

    Create status.js

---
+++ status.js
@@ -0,0 +1,10 @@
+{
+  "Adlinks":":green_square:",
+  "Codex":":green_square:",
+  "Delta":":green_square:",
+  "Fluxus":":green_square:",
+  "Hydrogen":":green_square:",
+  "Trigon":":green_square:",
+  "Vegax":":green_square:",
+  "HohoHub":":red_square:"
+}
