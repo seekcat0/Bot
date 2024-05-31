c2899320db76fea1ae19c1ffe6ad3fa2b58da225
Seek Cat <seekcat033@gmail.com>
2024-06-01 00:54:15 +0700

    Update status.js

--- status.js
+++ status.js
@@ -5,8 +5,8 @@
   "Delta":":green_square:",
   "Fluxus":":green_square:",
   "Hydrogen":":green_square:",
-  "Trigon":":green_square:",
-  "Vegax":":red_square:",
+  "Trigon":":red_square:",
+  "Vegax":":green_square:",
   "HohoHub":":red_square:",
   "ArceusX":":red_square:"
 }
