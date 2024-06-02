b29e3af46c15634e173ed7cf90cc693a0a850f7f
Seek Cat <seekcat033@gmail.com>
2024-06-03 03:07:08 +0700

    Update status.js

--- status.js
+++ status.js
@@ -1,12 +1,12 @@
 {
   "status": "success",
-  "Adlinks":"<a:trunoff:1246911668538310766>",
-  "Codex":"<a:trunoff:1246911668538310766>",
-  "Delta":"<a:turnon:1246911666013081732>",
-  "Fluxus":"<a:turnon:1246911666013081732>",
-  "Hydrogen":"<a:turnon:1246911666013081732>",
-  "Trigon":"<a:trunoff:1246911668538310766>",
-  "Vegax":"<a:turnon:1246911666013081732>",
-  "HohoHub":"<a:trunoff:1246911668538310766>",
-  "ArceusX":"<a:trunoff:1246911668538310766>"
+  "Adlinks":":red_square:",
+  "Codex":":red_square:",
+  "Delta":":green_square:",
+  "Fluxus":":green_square:",
+  "Hydrogen":":green_square:",
+  "Trigon":":red_square:",
+  "Vegax":":green_square:",
+  "HohoHub":":red_square:",
+  "ArceusX":":red_square:"
 }
