a62dae0e9ee464a95920434f38790af17b6404bb
Seek Cat <seekcat033@gmail.com>
2024-06-03 01:27:20 +0700

    Update status.js

--- status.js
+++ status.js
@@ -1,7 +1,7 @@
 {
   "status": "success",
-  "Adlinks":":green_square:",
-  "Codex":":yellow_square:",
+  "Adlinks":":red_square:",
+  "Codex":":red_square:",
   "Delta":":green_square:",
   "Fluxus":":green_square:",
   "Hydrogen":":green_square:",
